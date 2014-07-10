6018916b6762bdc970bfa06c5ad890fad1f99709
mrriving <mrriving@gmail.com>
2014-07-11 01:58:34 +0700

    Solved issue with warning message

--- zetta-app.js
+++ zetta-app.js
@@ -339,7 +339,9 @@ function Application(appFolder, appConfig) {
         self.app.set('views', path.join(appFolder,'views'));
         self.app.set('view engine', 'ejs');
         self.app.engine('html', require('ejs').renderFile);
-        self.app.use(require('body-parser')());//express.json());
+      //self.app.use(require('body-parser')());//express.json());
+        self.app.use(require('body-parser').urlencoded({ extended: true }));
+        self.app.use(require('body-parser').json());
         self.app.use(require('method-override')());
         self.app.use(require('cookie-parser')(self.app.sessionSecret));
 
@@ -350,7 +352,9 @@ function Application(appFolder, appConfig) {
                     secret: self.app.sessionSecret,
                     key: self.config.http.session.key,
                     cookie: self.config.http.session.cookie,
-                    store: self.app.sessionStore
+                    store: self.app.sessionStore,
+                    saveUninitialized: true,
+                    resave: true
                 }));
 
                 return callback();
