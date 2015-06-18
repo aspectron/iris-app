c3bcded750d664f4c50578a8b8a9663fb95028e1
surinder singh <surinder83singh@gmail.com>
2015-06-19 01:59:16 +0530

    account activation completed

--- zetta-app.js
+++ zetta-app.js
@@ -100,6 +100,46 @@ function getConfig(name) {
     return o;
 }
 
+function flash(options) {
+    options = options || {};
+    var safe = (options.unsafe === undefined) ? true : !options.unsafe;
+
+    return function(req, res, next) {
+        if (req.flash && safe) { return next(); }
+        req.flash = _flash;
+        req.redirectWithMsg = function(path, type, msg){
+            req.flash(type, msg);
+            res.redirect(path);
+        }
+        next();
+    }
+}
+
+function _flash(type, msg) {
+    if (this.session === undefined) throw Error('req.flash() requires sessions');
+    var msgs = this.session.flash = this.session.flash || {};
+    if (type && msg) {
+        // util.format is available in Node.js 0.6+
+        if (arguments.length > 2 && util.format) {
+            var args = Array.prototype.slice.call(arguments, 1);
+            msg = util.format.apply(undefined, args);
+        } else if (util.isArray(msg)) {
+            msg.forEach(function(val){
+                (msgs[type] = msgs[type] || []).push(val);
+            });
+            return msgs[type].length;
+        }
+        return (msgs[type] = msgs[type] || []).push(msg);
+    } else if (type) {
+        var arr = msgs[type];
+        delete msgs[type];
+        return arr || [];
+    } else {
+        this.session.flash = {};
+        return msgs;
+    }
+}
+
 function readJSON(filename) {
     if(!fs.existsSync(filename))
         return undefined;
@@ -437,6 +477,7 @@ function Application(appFolder, appConfig) {
         self.app.use(require('body-parser').json());
         self.app.use(require('method-override')());
         self.app.use(require('cookie-parser')(self.app.sessionSecret));
+        self.app.use(flash({unsafe: false}));
 
         if(self.config.mongodb) {
             var MongoStore = require('connect-mongo')(ExpressSession);
